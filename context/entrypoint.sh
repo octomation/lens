4da967777fe67affb41c76b93d3e3df606429887
Kamil Samigullin <kamil@samigullin.info>
2019-11-20 23:20:58 +0300

    fix #10: improve entrypoint script

--- context/entrypoint.sh
+++ context/entrypoint.sh
@@ -16,29 +16,29 @@ cert_name()  { echo "${SSL_PATH}/${fqdn:=$1}_le-crt.pem"; }
 chain_name() { echo "${SSL_PATH}/${fqdn:=$1}_le-chain-crt.pem"; }
 
 echo "normalizing..."
-    [ -n "${LE_ENABLED}" -a -n "${LE_EMAIL}" ]
+    [[ -n "${LE_ENABLED}" && -n "${LE_EMAIL}" ]]
     LE_ENABLED=$(($? == 0))
 
-    [ -n "${DEV_ENABLED}" ]
+    [[ -n "${DEV_ENABLED}" ]]
     DEV_ENABLED=$(($? == 0))
 
     HTTPS_ENABLED=$(($LE_ENABLED == 1 || $DEV_ENABLED == 1))
 
     echo "  environment:"
-    echo "    TIME_ZONE:    " $TIME_ZONE
-    echo "    LE_ENABLED:   " $LE_ENABLED
-    echo "    LE_EMAIL:     " $LE_EMAIL
-    echo "    DEV_ENABLED:  " $DEV_ENABLED
-    echo "    HTTPS_ENABLED:" $HTTPS_ENABLED
+    echo "    TIME_ZONE:    " ${TIME_ZONE}
+    echo "    LE_ENABLED:   " ${LE_ENABLED}
+    echo "    LE_EMAIL:     " ${LE_EMAIL}
+    echo "    DEV_ENABLED:  " ${DEV_ENABLED}
+    echo "    HTTPS_ENABLED:" ${HTTPS_ENABLED}
     echo ""
-    echo "    REPEAT_LOG:   " $REPEAT_LOG
-    echo "    SSL_PATH:     " $SSL_PATH
+    echo "    REPEAT_LOG:   " ${REPEAT_LOG}
+    echo "    SSL_PATH:     " ${SSL_PATH}
     echo "    DHPARAMS:     " ${SSL_PATH}/${DHPARAMS}
     echo "    DEV_CERT:     " ${SSL_PATH}/${DEV_CERT}
     echo "    DEV_KEY:      " ${SSL_PATH}/${DEV_KEY}
-    echo "    WEBROOT:      " $WEBROOT
-    echo "    CONFIG_PATH:  " $CONFIG_PATH
-    echo "    SITES_PATH:   " $SITES_PATH
+    echo "    WEBROOT:      " ${WEBROOT}
+    echo "    CONFIG_PATH:  " ${CONFIG_PATH}
+    echo "    SITES_PATH:   " ${SITES_PATH}
 echo "done"
 
 echo "setup timezone..."
@@ -48,11 +48,11 @@ echo "done"
 
 echo "copy configurations..."
 (
-    cd $SITES_PATH
+    cd ${SITES_PATH}
     for site in $(ls); do
-        file=$(basename $site .conf)
+        file=$(basename ${site} .conf)
         conf=${CONFIG_PATH}/${file}.conf
-        cp $site $conf
+        cp ${site} ${conf}
         echo "  ${SITES_PATH}/${site} copied to ${conf}"
     done
 )
@@ -60,16 +60,16 @@ echo "done"
 
 dhparam() {
     echo "make dhparams..."
-    if [ ! -f ${SSL_PATH}/${DHPARAMS} ]; then
+    if [[ ! -f ${SSL_PATH}/${DHPARAMS} ]]; then
         (
-            cd $SSL_PATH
-            openssl dhparam -out $DHPARAMS 2048
-            if [ $? -gt 0 ]; then
+            cd ${SSL_PATH}
+            openssl dhparam -out ${DHPARAMS} 2048
+            if [[ $? -gt 0 ]]; then
                 echo "  [CRITICAL] cannot generate Diffie-Hellman parameters"
                 echo "done"
                 return 1
             fi
-            chmod 600 $DHPARAMS
+            chmod 600 ${DHPARAMS}
         )
     else
         sleep 5 # give nginx time to start
@@ -80,17 +80,17 @@ dhparam() {
 
 generate() {
     echo "generate self-signed certificate..."
-    if [ ! -f ${SSL_PATH}/${DEV_CERT} -o ! -f ${SSL_PATH}/${DEV_KEY} ]; then
+    if [[ ! -f ${SSL_PATH}/${DEV_CERT} || ! -f ${SSL_PATH}/${DEV_KEY} ]]; then
         (
-            cd $SSL_PATH
-            openssl req -out $DEV_CERT -new -newkey rsa -keyout $DEV_KEY -config local.conf -x509 -days 365
-            if [ $? -gt 0 ]; then
+            cd ${SSL_PATH}
+            openssl req -out ${DEV_CERT} -new -newkey rsa -keyout ${DEV_KEY} -config local.conf -x509 -days 365
+            if [[ $? -gt 0 ]]; then
                 echo "  [CRITICAL] cannot generate self-signed certificate"
                 echo "done"
                 return 1
             fi
-            chmod 600 $DEV_CERT $DEV_KEY
-            openssl x509 -in $DEV_CERT -text -noout
+            chmod 600 ${DEV_CERT} ${DEV_KEY}
+            openssl x509 -in ${DEV_CERT} -text -noout
         )
     else
         echo "  skipped"
@@ -108,21 +108,21 @@ letsencrypt() {
     for alias in ${aliases[@]}; do
         domain="${domain} -d ${alias}"
     done
-    if [ $DEV_ENABLED -eq 1 ]; then
+    if [[ ${DEV_ENABLED} -eq 1 ]]; then
         domain="${domain} --test-cert --dry-run"
     fi
-    if [ ${#aliases[@]} -gt 0 ]; then
+    if [[ ${#aliases[@]} -gt 0 ]]; then
         echo "run certbot for ${fqdn} with aliases ${aliases}..."
     else
         echo "run certbot for ${fqdn}..."
     fi
-    local key=$(key_name $fqdn)
-    local certificate=$(cert_name $fqdn)
-    local chain=$(chain_name $fqdn)
-    echo "  expected key:              " $key
-    echo "  expected certificate:      " $certificate
-    echo "  expected certificate chain:" $chain
-    echo "  -d argument:               " $domain
+    local key=$(key_name ${fqdn})
+    local certificate=$(cert_name ${fqdn})
+    local chain=$(chain_name ${fqdn})
+    echo "  expected key:              " ${key}
+    echo "  expected certificate:      " ${certificate}
+    echo "  expected certificate chain:" ${chain}
+    echo "  -d argument:               " ${domain}
     mkdir -p ${WEBROOT}/.well-known/acme-challenge
     set -x
     certbot certonly -t -n \
@@ -131,10 +131,10 @@ letsencrypt() {
                      --email "${LE_EMAIL}" \
                      --webroot \
                      -w /usr/share/nginx/html \
-                     $domain
+                     ${domain}
     local result=$?
     set +x
-    if [ $result -gt 0 ]; then return $result; fi
+    if [[ ${result} -gt 0 ]]; then return ${result}; fi
     cp -fv /etc/letsencrypt/live/${fqdn}/privkey.pem   ${key}         || return $?
     cp -fv /etc/letsencrypt/live/${fqdn}/fullchain.pem ${certificate} || return $?
     cp -fv /etc/letsencrypt/live/${fqdn}/chain.pem     ${chain}       || return $?
@@ -147,16 +147,16 @@ enable_dev() {
         export SSL_KEY=${SSL_PATH}/${DEV_KEY}
         export SSL_CERT=${SSL_PATH}/${DEV_CERT}
 
-        cd $SITES_PATH
+        cd ${SITES_PATH}
         for site in $(grep -l * -e '#:https '); do
-            local file=$(basename $site .conf)
+            local file=$(basename ${site} .conf)
             local conf=${CONFIG_PATH}/${file}.conf
-            envsubst '${SSL_CERT} ${SSL_KEY}' < $site > $conf
-            sed -i "s|#:https ||g" $conf
-            sed -i "s|#:dev ||g" $conf
+            envsubst '${SSL_CERT} ${SSL_KEY}' < ${site} > ${conf}
+            sed -i "s|#:https ||g" ${conf}
+            sed -i "s|#:dev ||g" ${conf}
             nginx -t
-            if [ $? -gt 0 ]; then
-                cat $site > $conf
+            if [[ $? -gt 0 ]]; then
+                cat ${site} > ${conf}
                 echo "  [CRITICAL] configuration ${SITES_PATH}/${site} without HTTPS and DEV comments is invalid"
             else
                 echo "  configuration ${conf} is updated"
@@ -172,29 +172,31 @@ enable_prod() {
     local fqdn=$1
     echo "find configuration file for ${fqdn}..."
     (
-        export SSL_KEY=$(key_name $fqdn)
-        export SSL_CERT=$(cert_name $fqdn)
-        export SSL_CHAIN=$(chain_name $fqdn)
+        export SSL_KEY=$(key_name ${fqdn})
+        export SSL_CERT=$(cert_name ${fqdn})
+        export SSL_CHAIN=$(chain_name ${fqdn})
 
-        cd $SITES_PATH
+        cd ${SITES_PATH}
         local site=''
-        if [ -f $fqdn ]; then
-            site=$fqdn
-        elif [ -f ${fqdn}.conf ]; then
+        if [[ -f ${fqdn} ]]; then
+            site=${fqdn}
+        elif [[ -f ${fqdn}.conf ]]; then
             site=${fqdn}.conf
+        elif find . -type f -print | xargs grep ${fqdn}; then
+            site=$(find . -type f -print0 | xargs -0 grep -l ${fqdn} | xargs basename)
         else
-            echo "  [CRITICAL] cannot find neither ${fqdn} nor ${fqdn}.conf in ${SITES_PATH}"
+            echo "  [CRITICAL] cannot find ${fqdn} in ${SITES_PATH}"
             echo "done"
             return 1
         fi
-        local file=$(basename $site .conf)
+        local file=$(basename ${site} .conf)
         local conf=${CONFIG_PATH}/${file}.conf
-        envsubst '${SSL_CERT} ${SSL_KEY} ${SSL_CHAIN}' < $site > $conf
-        sed -i "s|#:https ||g" $conf
-        sed -i "s|#:le ||g" $conf
+        envsubst '${SSL_CERT} ${SSL_KEY} ${SSL_CHAIN}' < ${site} > ${conf}
+        sed -i "s|#:https ||g" ${conf}
+        sed -i "s|#:le ||g" ${conf}
         nginx -t
-        if [ $? -gt 0 ]; then
-            cat $site > $conf
+        if [[ $? -gt 0 ]]; then
+            cat ${site} > ${conf}
             echo "  [CRITICAL] configuration ${SITES_PATH}/${site} without HTTPS and LE comments is invalid"
             echo "done"
             return 1
@@ -209,26 +211,26 @@ process() {
     #      ${@:1}
     # specifications...
     # - where specification is domain:alias,...
-    if [ $HTTPS_ENABLED -eq 0 ]; then
+    if [[ ${HTTPS_ENABLED} -eq 0 ]]; then
         echo "  HTTPS is disabled"
         return 0
     fi
     dhparam || return $?
-    if [ $LE_ENABLED -eq 0 ]; then
+    if [[ ${LE_ENABLED} -eq 0 ]]; then
         echo "  [WARNING] let's encrypt is disabled"
         echo "  [WARNING] environment will be configured for local development"
         generate   || return $?
         enable_dev || return $?
     else
         for spec in $@; do
-            local fqdn=$(echo    $spec | awk -F ':' '{print $1}')
-            local aliases=$(echo $spec | awk -F ':' '{print $2}' | tr ',' ' ')
-            letsencrypt $fqdn $aliases
-            if [ $? -gt 0 ]; then
+            local fqdn=$(echo    ${spec} | awk -F ':' '{print $1}')
+            local aliases=$(echo ${spec} | awk -F ':' '{print $2}' | tr ',' ' ')
+            letsencrypt ${fqdn} ${aliases}
+            if [[ $? -gt 0 ]]; then
                 echo "  [CRITICAL] cannot process ${fqdn} certificate"
             else
-                enable_prod $fqdn
-                if [ $? -gt 0 ]; then
+                enable_prod ${fqdn}
+                if [[ $? -gt 0 ]]; then
                     echo "  [CRITICAL] cannot set up configuration for ${fqdn}"
                 fi
             fi
@@ -236,10 +238,10 @@ process() {
     fi
     nginx -s reload
     local result=$?
-    if [ $result -gt 0 ]; then
+    if [[ ${result} -gt 0 ]]; then
         echo "  [CRITICAL] cannot reload nginx"
     fi
-    return $result
+    return ${result}
 }
 
 wrapped_process() {
@@ -247,7 +249,7 @@ wrapped_process() {
     process $@
     local result=$?
     echo "done"
-    return $result
+    return ${result}
 }
 
 watch() {
@@ -261,24 +263,24 @@ watch() {
 case $1 in
     renew)
         echo "[TODO] force renew..."
-        ;;
+    ;;
     repeat)
-        if [ ! -f $REPEAT_LOG ]; then
+        if [[ ! -f ${REPEAT_LOG} ]]; then
             echo '[CRITICAL] nothing to repeat'
             exit 1
         fi
-        wrapped_process $(cat $REPEAT_LOG)
-        ;;
+        wrapped_process $(cat ${REPEAT_LOG})
+    ;;
     process)
         wrapped_process ${@:2}
-        ;;
+    ;;
     *)
-        if [ ${#@} -gt 0 ]; then
-            echo $@ > $REPEAT_LOG
+        if [[ ${#@} -gt 0 ]]; then
+            echo $@ > ${REPEAT_LOG}
             echo "${@} copied to ${REPEAT_LOG}"
         fi
         (wrapped_process $@ && watch) &
         echo "start nginx..."
         nginx -g "daemon off;"
-        ;;
+    ;;
 esac
